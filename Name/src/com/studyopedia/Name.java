d0f0257cf352fe45e426ca128fc1151eed98d062
user <user@10.107.30.180>
2023-10-16 18:35:46 +0800

    First Commit

---
+++ Name/src/com/studyopedia/Name.java
@@ -0,0 +1,12 @@
+package com.studyopedia;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello my name is qaisara");
+
+	}
+
+}
